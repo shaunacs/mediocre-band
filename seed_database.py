1b06d8aae54295198a6162ed876efb846ead0f4b
Shauna Saunders <shaunacsaunders@outlook.com>
2021-10-12 20:58:12 -0400

    Addd commands to drop and create db to seed file

--- seed_database.py
+++ seed_database.py
@@ -1,3 +1,10 @@
 """Script to seed fake database for MediocreBand"""
 
 import os
+import model
+import server
+
+os.system('dropdb mediocre')
+os.system('createdb mediocre')
+model.connect_to_db(server.app)
+model.db.create_all()
