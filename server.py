23b3bf8911dd630bcdf581fd47c1e8a2241125ee
Shauna Saunders <shaunacsaunders@outlook.com>
2021-10-18 17:59:09 -0400

    Create root server route

--- server.py
+++ server.py
@@ -1,7 +1,18 @@
 """Server for MediocreBand app"""
 
-from flask import Flask
+from flask import Flask, render_template
 from model import connect_to_db
 
 app = Flask(__name__)
 app.secret_key = "dev"
+
+@app.route('/')
+def render_homepage():
+    """Renders MediocreBand homepage"""
+
+    return render_template('homepage.html')
+
+
+if __name__ == '__main__':
+    connect_to_db(app)
+    app.run(debug=True, use_reloader=True, use_debugger=True)
